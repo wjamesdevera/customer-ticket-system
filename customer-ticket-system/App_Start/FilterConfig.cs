ad96cb661494ace770412ab5556489eb610f5b1a
kody <w.jamesdevera@gmail.com>
2024-11-09 17:03:24 +0800

    Add project files.

---
+++ customer-ticket-system/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace customer_ticket_system
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ customer-ticket-system/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="customer_ticket_system.MvcApplication" Language="C#" %>
